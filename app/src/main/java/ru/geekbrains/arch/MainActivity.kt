a913b351abe85fb2a8498eb73356b5653fb0e635
Aleksey <alexcorpetf@gmail.com>
2021-09-15 22:54:43 +1000

    Some tuning

--- app/src/main/java/ru/geekbrains/arch/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/arch/MainActivity.kt
@@ -1,11 +1,38 @@
 package ru.geekbrains.arch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import ru.geekbrains.arch.R.layout.activity_main
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(activity_main), MainView {
+
+    val presenter = MainPresenter(this)
+
+    private val btn_counter1: Button by lazy { findViewById(R.id.btn_counter1) }
+    private val btn_counter2: Button by lazy { findViewById(R.id.btn_counter2) }
+    private val btn_counter3: Button by lazy { findViewById(R.id.btn_counter3) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val listener = View.OnClickListener {
+            presenter.counterClick(it.id)
+        }
+
+        btn_counter1.setOnClickListener(listener)
+        btn_counter2.setOnClickListener(listener)
+        btn_counter3.setOnClickListener(listener)
     }
+
+    //Подсказка к ПЗ: поделить на 3 отдельные функции и избавиться от index
+    override fun setButtonText(index: Int, text: String) {
+        when(index){
+            0 -> btn_counter1.text = text
+            1 -> btn_counter2.text = text
+            2 -> btn_counter3.text = text
+        }
+    }
+
 }
---
+++ app/src/main/java/ru/geekbrains/arch/MainView.kt
@@ -0,0 +1,5 @@
+package ru.geekbrains.arch
+
+interface MainView {
+    fun setButtonText(index: Int, text: String)
+}
